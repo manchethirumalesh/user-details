a6da47a40ac4e8ae2670bc8241af34c29eea0d0d
Thirumalesh <manchethirumalesh@gmail.com>
2021-04-19 19:20:48 +0530

    c2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { DisplayUserComponent } from './display-user/display-user.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { Ng2OrderModule } from 'ng2-order-pipe';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { C1Component } from './c1/c1.component'
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component'
  
 @NgModule({
   declarations: [
     AppComponent,
     DisplayUserComponent,
-    C1Component
+    C1Component,
+    C2Component
   ],
   imports: [
     BrowserModule,
